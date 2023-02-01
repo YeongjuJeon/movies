a8d9521f4bc1f8d9310c4f6c648f42110978a562
yj620 <yeongju620@gmail.com>
2023-02-01 11:36:49 +0900

    2번째 백업

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
 import './App.css';
 import React, { Component } from 'react';
+import Home from './routes/Home';
+import About from './routes/About';
+import { Routes, Route, BrowserRouter } from 'react-router-dom';
+import Navigation from './component/Navigation';
+import Detail from './routes/Detail';
 
-class App extends Component {
-  state = {
-    isLoading: true,
-    movies: [],
-  }
-  componentDidMount(){
-    setTimeout(()=>{
-      this.setState({isLoading: false})
-    },5000)
-  }
-  render() {
-    const {isLoading} = this.state;
-    return (
-      <div>
-        {isLoading ? 'Loading...' : 'We are ready'}
-      </div>
-    );
-  }
-}
+
+const App = () => {
+  return (
+    <div>
+      <BrowserRouter>
+        <Navigation/>
+        <Routes>
+          <Route path='/' element={<Home/>} />
+          <Route path='/About' element={<About/>} />
+          <Route path='/detail/:id' element={<Detail/>}/>
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
+};
 
 export default App;
---
+++ src/component/Navigation.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './Navigation.scss';
+
+const Navigation = () => {
+    return (
+        <div className='nav'>
+            <ul>
+                <li><Link to="/">Home</Link></li>
+                <li><Link to="/About">About</Link></li>
+            </ul>
+        </div>
+    );
+};
+
+
+export default Navigation;
